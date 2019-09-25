c4dd3137b7e5c1f906f9f968c14361626c097175
Charles Broskoski <broskoski@gmail.com>
2019-09-25 18:28:30 -0400

    Remove selection rules

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -2,15 +2,6 @@ import { createGlobalStyle } from "styled-components"
 import styled from "styled-components"
 
 const Style = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-  
-  ::-moz-selection { background: #000; color: #fff; }
-  ::selection { background: #000; color: #fff; }
-
   html {
     font-family: 'Arial';
     font-size: 14px;
